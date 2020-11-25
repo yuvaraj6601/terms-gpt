44e3ff41a141e75a6a34ba2d37592e1358c34a05
Kishore Padmanaban <kishorepadmanaban@gmail.com>
2020-11-25 17:01:56 +0530

    portal added

---
+++ src/common_components/hoc/portal.hoc.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+const portalRoot: any = document.getElementById("portal");
+
+export default class Portal extends React.Component <{}, {}>{
+  constructor(public props: any, public el: HTMLElement) {
+    super(props);
+    // 1: Create a new div that wraps the component
+    this.el = document.createElement("div");
+  }
+  // 2: Append the element to the DOM when it mounts
+  componentDidMount = () => {
+    portalRoot.appendChild(this.el);
+  };
+  // 3: Remove the element when it unmounts
+  componentWillUnmount = () => {
+    portalRoot.removeChild(this.el);
+  };
+  render() {
+    // 4: Render the element's children in a Portal
+    const { children } = this.props;
+    return ReactDOM.createPortal(children, this.el);
+  }
+}
